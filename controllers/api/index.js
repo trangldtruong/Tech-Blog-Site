03e13b2c2343ed99374e41364a24a8f94bc71bee
Trang Truong <trangldtruong@gmail.com>
2024-02-14 19:42:43 -0500

    fix public js and css

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,6 +3,6 @@ const userRoutes = require('./userRoutes');
 const postRoutes = require('../homeRoutes');
 
 router.use('/users', userRoutes);
-router.use('/post', postRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
